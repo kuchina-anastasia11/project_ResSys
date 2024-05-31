1f82e1690f7f3aa9d7bb8cb0d5754932b14f38a6
anpalmak2003 <73543260+anpalmak2003@users.noreply.github.com>
2024-05-31 19:32:58 +0300

    Update modeling.py

--- finetune/modeling.py
+++ finetune/modeling.py
@@ -8,6 +8,10 @@ from torch import nn, Tensor
 from transformers import AutoModel
 from transformers.file_utils import ModelOutput
 
+import numpy as np 
+from .losses import angle_loss, FocalLoss
+import torch.nn.functional as F
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,13 +32,32 @@ class BiEncoderModel(nn.Module):
                  sentence_pooling_method: str = 'cls',
                  negatives_cross_device: bool = False,
                  temperature: float = 1.0,
-                 use_inbatch_neg: bool = True
+                 use_inbatch_neg: bool = True,
+                 loss: str = 'cross_entropy',
+                 focal_alpha=None, focal_gamma=None
                  ):
         super().__init__()
         self.model = AutoModel.from_pretrained(model_name)
-        self.cross_entropy = nn.CrossEntropyLoss(reduction='mean')
+        self.loss = loss
 
+        if loss == "cross_entropy" or loss == "cross_entropy_t_b":
+            self.loss_f = nn.CrossEntropyLoss(reduction='mean')
+            if loss == "cross_entropy_t_b":
+                self.temperature_l = nn.Parameter(data=torch.tensor(np.log(5), dtype=torch.float32, requires_grad=True))
+                self.bias = nn.Parameter(data=torch.tensor(-5, dtype=torch.float32, requires_grad=True))
+        if loss == "angle":
+            self.loss_f = angle_loss
+        if loss == "sigmoid" or loss == "sigmoid2":
+            self.temperature_l = nn.Parameter(data=torch.tensor(np.log(5), dtype=torch.float32, requires_grad=True))
+            self.bias = nn.Parameter(data=torch.tensor(-5, dtype=torch.float32, requires_grad=True))
+            if loss == 'sigmoid2':
+                normlized = False
+        if self.loss == 'focal':
+            self.loss_f = FocalLoss(alpha= focal_alpha, gamma = focal_gamma, reduction = 'mean')
+
+        #self.cross_entropy = nn.CrossEntropyLoss(reduction='mean')
         self.normlized = normlized
+        
         self.sentence_pooling_method = sentence_pooling_method
         self.temperature = temperature
         self.use_inbatch_neg = use_inbatch_neg
@@ -89,19 +112,77 @@ class BiEncoderModel(nn.Module):
                 p_reps = self._dist_gather_tensor(p_reps)
 
             group_size = p_reps.size(0) // q_reps.size(0)
-            if self.use_inbatch_neg:
-                scores = self.compute_similarity(q_reps, p_reps) / self.temperature # B B*G
-                scores = scores.view(q_reps.size(0), -1)
 
-                target = torch.arange(scores.size(0), device=scores.device, dtype=torch.long)
-                target = target * group_size
-                loss = self.compute_loss(scores, target)
-            else:
-                scores = self.compute_similarity(q_reps[:, None, :,], p_reps.view(q_reps.size(0), group_size, -1)).squeeze(1) / self.temperature # B G
+            if self.loss in ["cross_entropy", "cross_entropy_t_b", "focal"]:
+                if self.use_inbatch_neg:
+                    if self.loss == "cross_entropy" or self.loss == "focal":
+                        scores = self.compute_similarity(q_reps, p_reps) / self.temperature # B B*G
+                    if self.loss == "cross_entropy_t_b":
+                        t = torch.exp(self.temperature_l)
+                        scores = self.compute_similarity(q_reps, p_reps) * t + self.bias 
+                    scores = scores.view(q_reps.size(0), -1)
 
+                    target = torch.arange(scores.size(0), device=scores.device, dtype=torch.long)
+                    target = target * group_size
+                    loss = self.compute_loss(scores, target)
+                else:
+                    if self.loss == "cross_entropy" or self.loss == "focal":
+                        scores = self.compute_similarity(q_reps[:, None, :,], p_reps.view(q_reps.size(0), group_size, -1)).squeeze(1) / self.temperature # B G
+                    if self.loss == "cross_entropy_t_b":
+                        t = torch.exp(self.temperature_l)
+                        scores = self.compute_similarity(q_reps[:, None, :,], p_reps.view(q_reps.size(0), group_size, -1)).squeeze(1) * t + self.bias 
+                    scores = scores.view(q_reps.size(0), -1)
+                    target = torch.zeros(scores.size(0), device=scores.device, dtype=torch.long)
+                    loss = self.compute_loss(scores, target)
+            if self.loss == "sigmoid":
+                b = q_reps.shape[0]
+                t = torch.exp(self.temperature_l)
+                scores = self.compute_similarity(q_reps, p_reps) * t + self.bias # B B*G
                 scores = scores.view(q_reps.size(0), -1)
-                target = torch.zeros(scores.size(0), device=scores.device, dtype=torch.long)
-                loss = self.compute_loss(scores, target)
+                scores_z = -scores
+                j = 0
+                for i in range(0, scores.shape[1], group_size):
+                    scores_z[j][i]*=-1
+                    j+=1
+                loss = -torch.sum(F.logsigmoid(scores_z)) / b
+                scores = self.compute_similarity(q_reps, p_reps)
+            if self.loss == "sigmoid2":
+                b = q_reps.shape[0]
+                t = torch.exp(self.temperature_l)
+                scores = self.compute_similarity(q_reps, p_reps) * t + self.bias # B B*G
+                scores = scores.view(q_reps.size(0), -1)
+                mask = torch.zeros(scores.shape, dtype=torch.bool)
+                j = 0
+                for i in range(0, scores.shape[1], group_size):
+                    mask[j][i]=True
+                    j+=1
+                pos = torch.stack(scores[mask].chunk(b), dim=0)
+                neg = torch.stack(scores[~mask].chunk(b), dim=0)
+                neg = -torch.stack(torch.log(torch.sum(torch.exp(neg),dim=1)).chunk(b), dim=0)
+                loss = -torch.sum(F.logsigmoid(torch.hstack((pos,neg)))) / b
+                scores = self.compute_similarity(q_reps, p_reps)
+            if self.loss == 'angle':
+                batch_size = q_reps.size(0)
+                mask_pos = group_size*torch.arange(batch_size,  dtype=torch.long)
+                positive = p_reps[mask_pos]
+                positive = torch.stack((torch.flatten(positive.unsqueeze(1).expand(-1, group_size-1, -1)).chunk((group_size-1)*batch_size)),dim=0)
+                mask_neg = torch.arange(batch_size*group_size,  dtype=torch.long)
+                mask_neg = mask_neg[(mask_neg[:, None] != mask_pos).all(dim=1)]
+                negative = p_reps[mask_neg]
+                text = q_reps
+                text = torch.stack((torch.flatten(text.unsqueeze(1).expand(-1, group_size-1, -1)).chunk((group_size-1)*batch_size)),dim=0)
+                #positive, negative = torch.chunk(p_reps, 2, dim=0)
+                assert text.shape == positive.shape == negative.shape, f'text.shape={text.shape}, postive.shape={positive.shape}, negative.shape={negative.shape}'
+                _, fea_dim = q_reps.shape
+                positive_inputs = torch.stack((text, positive), dim=1).reshape(-1, fea_dim)  # zip(text, positive), tensor (bath*2,seq_len) 
+                positive_labels = torch.ones_like(positive_inputs[:, :1]).long()
+                negative_inputs = torch.stack((text, negative), dim=1).reshape(-1, fea_dim)  # zip(text, negative)
+                negative_labels = torch.zeros_like(negative_inputs[:, :1]).long()
+                combined_inputs = torch.cat((positive_inputs, negative_inputs), dim=0)
+                combined_labels = torch.cat((positive_labels, negative_labels), dim=0)
+                loss = self.compute_loss(combined_inputs, combined_labels)
+                scores = self.compute_similarity(q_reps, p_reps)
+
 
         else:
             scores = self.compute_similarity(q_reps, p_reps)
@@ -114,7 +195,7 @@ class BiEncoderModel(nn.Module):
         )
 
     def compute_loss(self, scores, target):
-        return self.cross_entropy(scores, target)
+        return self.loss_f(scores, target)
 
     def _dist_gather_tensor(self, t: Optional[torch.Tensor]):
         if t is None:
